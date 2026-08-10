65e392a1f41b005eb8016189f924b6e245da369c
Aryan <aryanshah0514@gmail.com>
2026-08-10 14:04:53 +0200

    stash pop cv files

---
+++ src/utils/handleMouseGlow.ts
@@ -0,0 +1,10 @@
+import { MouseEvent } from "react";
+
+export const handleMouseGlow = (e: MouseEvent<HTMLElement>) => {
+  const target = e.currentTarget;
+  const rect = target.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  target.style.setProperty("--mouse-x", `${x}px`);
+  target.style.setProperty("--mouse-y", `${y}px`);
+};
